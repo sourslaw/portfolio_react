c9ae98f9d145c52600ead7ac0c66b60040b68919
Scott Lawan <scottlawan@Scotts-Mac-mini.local>
2021-08-02 22:26:22 -0600

    updating resume component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
     <Navbar className={classes.navBarMargin} bg="light" expand="lg">
       <Container className={classes.font}>
         <Nav >
-          <Nav.Link className={classes.footer} href='https://github.com/sourslaw'><i class="fab fa-github-alt fa-2x"></i></Nav.Link>
-          <Nav.Link className={classes.footer} href='https://www.linkedin.com/in/scottlawan/'><i class="fab fa-linkedin-in fa-2x"></i></Nav.Link>
-          <Nav.Link className={classes.footer} href='https://www.instagram.com/scottlawan/'><i class="fab fa-instagram fa-2x"></i></Nav.Link>
+          <Nav.Link className={classes.footer} href='https://github.com/sourslaw' target="blank"><i class="fab fa-github-alt fa-2x"></i></Nav.Link>
+          <Nav.Link className={classes.footer} href='https://www.linkedin.com/in/scottlawan/' target="blank"><i class="fab fa-linkedin-in fa-2x"></i></Nav.Link>
+          <Nav.Link className={classes.footer} href='https://www.instagram.com/scottlawan/' target="blank"><i class="fab fa-instagram fa-2x"></i></Nav.Link>
         </Nav>
 
         <Nav className={classes.bitsCopyright}>
--- src/components/layout/Resume/ResumeItem.js
+++ src/components/layout/Resume/ResumeItem.js
@@ -1,37 +1,20 @@
 import React from "react";
-import classes from './ResumeItem.module.css';
-
 import { Col, Row } from "react-bootstrap";
+import classes from './ResumeItem.module.css';
 
 
 function ResumeItem(props) {
-  // return (
-  //   <li>
-  //     <p className={classes.what}>{props.what}</p>
-  //     <p className={classes.where}>{props.where}</p>
-  //     <p className={classes.when}>{props.when}</p>
-  //     <p className={classes.description}>{props.description}</p>
-  //   </li>
-	// );
-	
-	// TEST
 	return (
-		// <Col>
-
     <Row>
-			<Col className={classes.col1}  >
-			<p className={classes.when}>{props.when}</p>
+			<Col className={classes.col1} >
+			  <p className={classes.when}>{props.when}</p>
 			</Col>
 			<Col>
-			<p className={classes.what}>{props.what}</p>
-      <p className={classes.where}>{props.where}</p>
-      
-      <p className={classes.description}>{props.description}</p>
+        <p className={classes.what}>{props.what}</p>
+        <p className={classes.where}>{props.where}</p>
+        <p className={classes.description}>{props.description}</p>
 			</Col>
-
     </Row>
-		// </Col>
-
   );
 }
 
