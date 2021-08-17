2fa59ca0759d5019e04c38ad732de87847142db7
sourslaw <lawanscott@gmail.com>
2021-08-17 14:09:09 -0600

    updating portfolio comp.

--- src/components/layout/Portfolio/PortfolioContent.js
+++ src/components/layout/Portfolio/PortfolioContent.js
@@ -9,10 +9,11 @@ function PortfolioContent(props) {
   return (
 		<Container fluid className={classes.container}>
 			<div className={classes.headingCard}>portfolio</div>
+
       <Container className={classesPort.container}>
         <div className={classesPort.mainRow}>
           <Row >
-            <Col sm={6} className={classesPort.ul}>
+            <Col className={classesPort.ul}>
               {props.contents.slice(0,2).map((content) => 
                 <PortfolioItem 
                   key={content.id}
@@ -25,9 +26,23 @@ function PortfolioContent(props) {
               )}
             </Col>
           </Row>
-          <Row>
-            <Col sm={6} className={classesPort.ul}>
+          <Row >
+            <Col className={classesPort.ul}>
               {props.contents.slice(2,4).map((content) => 
+                <PortfolioItem 
+                  key={content.id}
+                  title={content.title}
+                  image={content.image}
+                  description={content.description}
+                  link={content.link}
+                  linkTwo={content.linkTwo}
+                />
+              )}
+            </Col>
+          </Row>
+					<Row >
+            <Col className={classesPort.ul}>
+              {props.contents.slice(4,6).map((content) => 
                 <PortfolioItem 
                   key={content.id}
                   title={content.title}
