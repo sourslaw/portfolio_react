86d9429feb4df8fb1adc95899f8ae3eace5defd1
sourslaw <lawanscott@gmail.com>
2021-08-17 22:26:48 -0600

    updating about summary, updating contact page responsiveness

--- src/components/layout/About/AboutContent.js
+++ src/components/layout/About/AboutContent.js
@@ -17,7 +17,7 @@ function AboutContent() {
         <Col xs lg="6" className={classes.col2}>
           <div>full stack developer</div>
           <h1 className={classes.font}>scott lawan</h1>
-          <p>I am a creative and passionate information professional and software developer. I am seeking to grow my skill set through new challenges and the creation of thoughtful tools for thoughtful use.</p>
+          <p>Full stack web developer. Graduate degree and professional experience in information science and data management. Proven track record in team-building, mentorship, and project management. Passionate about problem solving, collaboration, and continuous learning.</p>
           <a href="https://drive.google.com/file/d/12NNTVp1QhOEINZRFQ2smujnbTpq2hhE9/view?usp=sharing" target="blank"><Button variant="outline-secondary" className={classes.button}>download CV</Button></a>
           <a href='/contact'><Button variant="outline-secondary" className={classes.button}>contact</Button></a>
         </Col>
