827df0b389d9e9d7ee54f215f4d5a4dfa4e8d508
sourslaw <lawanscott@gmail.com>
2023-02-11 15:17:47 -0600

    updated blog page margin, updating contact layout

--- src/components/layout/Contact/ContactContent.js
+++ src/components/layout/Contact/ContactContent.js
@@ -58,40 +58,32 @@ export const ContactForm = (props) => {
 	return (
 		<Container fluid className={classes.container}>
 			<div className={classes.headingCard}>contact</div>
-
 			<Container>
 				<Row className={classesContact.map}>
-					<Iframe src="https://www.google.com/maps/embed/v1/place?q=minneapolis,+mn&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" />
+					<Iframe className={classesContact.iframe} src="https://www.google.com/maps/embed/v1/place?q=minneapolis,+mn&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" />
 				</Row>
 				<Row className={classesContact.contact}>
-					<Col xs lg={3}>
-						<Row>
-							<Col lg={2}>
-								<span className={classesContact.icons}>
-									<i class="far fa-compass"></i>
-								</span>
-							</Col>
-							<Col className={classesContact.col1B}>
-								<p>minneapolis, mn, united states</p>
-							</Col>
-						</Row>
-						<Row>
-							<Col lg={2}>
-								<span className={classesContact.icons}>
-									<i class="far fa-envelope"></i>
-								</span>
-							</Col>
-							<Col className={classesContact.col1B}>
-								<p><a className={classesContact.link} href="mailto:lawanscott@gmail.com" alt="">Lawanscott@gmail.com</a></p>
-							</Col>
-						</Row>
+
+					<Col lg={4}>
+						<Col>
+							<span className={classesContact.icons}>
+								<i class="far fa-compass"></i>
+							</span>
+							<p className={classesContact.col1B}>Minneapolis, MN</p>
+						</Col>
+						<Col>
+							<span className={classesContact.icons}>
+								<i class="far fa-envelope"></i>
+							</span>
+							<p className={classesContact.col1B}><a className={classesContact.link} href="mailto:lawanscott@gmail.com" alt="">Lawanscott@gmail.com</a></p>
+						</Col>
 					</Col>
 					
-					<Col className={classesContact.bottomSpace}>
+					<Col lg={8} className={classesContact.bottomSpace}>
 						<Form id="form">
 						{/* <Form noValidate validated={validated} id="form"> */}
 							<Row>
-								<Col xs={12} lg={5}>
+								<Col xs={12}>
 								<h5>please reach out . . .</h5>
 									<Form.Group as={Col} controlId="formGridName">
 										<Form.Control className="mt-3 mb-3" onChange= {handleChange} name="name" type="name" placeholder="full name" />
@@ -102,8 +94,6 @@ export const ContactForm = (props) => {
 									<Form.Group as={Col} controlId="formGridMobile">
 										<Form.Control onChange= {handleChange} name="subject" placeholder="subject" />
 									</Form.Group>
-								</Col>
-								<Col>
 									<Form.Group as={Col} id="formGridQuery">
 										<Form.Control className="mb-3" onChange= {handleChange} name="message" as="textarea" placeholder="message" style={{ height: '175px', marginTop: '1rem' }}  />
 									</Form.Group>
@@ -112,6 +102,7 @@ export const ContactForm = (props) => {
 							<Button onClick={handleSubmit} variant="outline-secondary" type="submit">submit</Button>
 						</Form >
 					</Col>
+					
 				</Row>
 			</Container>
 		</Container>
