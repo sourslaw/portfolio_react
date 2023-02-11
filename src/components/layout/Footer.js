8dfbc587a5d1a59351bb851a661456cd76682a54
sourslaw <lawanscott@gmail.com>
2023-02-10 22:14:02 -0600

    Updated copyright, updated resume

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
         </Nav>
 
         <Nav className={classes.bitsCopyright}>
-          <p>&#169;2021 all rights reserved. scott lawan</p>
+          <p>&#169;2023 all rights reserved. scott lawan</p>
         </Nav>
       </Container>
     </Navbar>
